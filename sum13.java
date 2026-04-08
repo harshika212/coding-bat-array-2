2a798b17d6e64a34d4143e0f7f9585ee9e0ec11a
harshika212 <harshikasingh212@gmail.com>
2026-04-08 23:52:06 +0530

    Update and rename sum to sum13.java

--- sum13.java
+++ sum13.java
@@ -5,3 +5,17 @@ Return the sum of the numbers in the array, returning 0 for an empty array. Exce
 sum13([1, 2, 2, 1]) → 6
 sum13([1, 1]) → 2
 sum13([1, 2, 2, 1, 13]) → 6
+
+public int sum13(int[] nums) {
+  int sum = 0;
+
+  for (int i = 0; i < nums.length; i++) {
+    if (nums[i] == 13) {
+      i++; // skip next element also
+    } else {
+      sum += nums[i];
+    }
+  }
+
+  return sum;
+}
