58c64a32cd52eea9e51745da9db512f71e3c97a7
harshika212 <harshikasingh212@gmail.com>
2026-04-12 23:51:37 +0530

    Update centeredAverage.java

--- centeredAverage.java
+++ centeredAverage.java
@@ -5,3 +5,16 @@
 // centeredAverage([1, 2, 3, 4, 100]) → 3
 // centeredAverage([1, 1, 5, 5, 10, 8, 7]) → 5
 // centeredAverage([-10, -4, -2, -4, -2, 0]) → -3
+
+
+  int sum = 0;
+  int min = nums[0];
+  int max = nums[0];
+
+  for (int i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    min = Math.min(min, nums[i]);
+    max = Math.max(max, nums[i]);
+  }
+
+  return (sum - min - max) / (nums.length - 2);
