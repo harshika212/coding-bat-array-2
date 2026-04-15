ae856e34b5c1b436c4a5d593b80b90d28e443d95
harshika212 <harshikasingh212@gmail.com>
2026-04-15 23:47:13 +0530

    Update more14.java

--- more14.java
+++ more14.java
@@ -4,3 +4,13 @@
 // more14([1, 4, 1]) → true
 // more14([1, 4, 1, 4]) → false
 // more14([1, 1]) → true
+
+int count1 = 0;
+  int count4 = 0;
+
+  for (int num : nums) {
+    if (num == 1) count1++;
+    if (num == 4) count4++;
+  }
+
+  return count1 > count4;
