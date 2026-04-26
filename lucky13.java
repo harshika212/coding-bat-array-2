0be50fdb67d7ae6e55fbb7f92e1feb2173528e07
harshika212 <harshikasingh212@gmail.com>
2026-04-26 23:40:37 +0530

    Update lucky13.java

--- lucky13.java
+++ lucky13.java
@@ -4,3 +4,10 @@
 // lucky13([0, 2, 4]) → true
 // lucky13([1, 2, 3]) → false
 // lucky13([1, 2, 4]) → false
+
+public boolean lucky13(int[] nums) {
+  for(int i=0;i<nums.length;i++){
+    if(nums[i]==1 || nums[i]==3) return false;
+  }
+  return true;
+}
